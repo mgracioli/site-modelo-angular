da4e3430e5890da9387a7226dfef07e104fbf65f
mgracioli <michel-rg@hotmail.com>
2021-09-27 23:40:09 -0300

    continuação da implementação do scroll-carousel

--- src/app/shared/components/scroll-carrousel/scroll-carrousel.component.ts
+++ src/app/shared/components/scroll-carrousel/scroll-carrousel.component.ts
@@ -14,8 +14,7 @@ export class ScrollCarrouselComponent implements OnInit {
     price: '',
     storeName: ''
   }
-
-  card!: any[]
+  movePx: number = 0;
 
   constructor() { }
 
@@ -27,10 +26,32 @@ export class ScrollCarrouselComponent implements OnInit {
     this.cardProduct.storeName = 'Electronics & More'
   }
 
-  moveProducts(){
-    console.log('moveu')
-    //this.card = document.getElementsByClassName('carousel-card')
-    //this.card.style.left=5+"px";
+  moveProducts(side: string){
+    const carouselList = document.getElementById('carousel-list')
+    
+    if(side == "left"){
+      this.movePx-=450
+
+      if((carouselList!.offsetWidth + this.movePx) > 0){
+        //console.log("width do carossel: "+carouselList?.offsetWidth)
+        //console.log("movePx: "+this.movePx)
+        console.log("movePx+carList: "+carouselList!.offsetWidth + this.movePx)
+        carouselList!.style.left = this.movePx+"px"
+      }else{
+        this.movePx = carouselList!.offsetWidth;  //0 para travar o card-list senão ele vai ficar aumentando o valor de this.movePx e, quando clicar no botão de scroll-left, ele vai reposicionar a página para esse offset que estaria fora da lista (esse offset poderia ser infinito)
+        console.log("movePx final: "+this.movePx)
+      }    
+    }else if(side == "right"){
+      this.movePx+=450
+
+      //offsetLeft retorna a distancia, em pixels, do carousel-list com relação ao carousel-frame
+      if(carouselList!.offsetLeft != 0){
+        carouselList!.style.left = this.movePx+"px"
+      }else{
+        this.movePx = 0;  //0 para travar o card-list senão ele vai ficar aumentando o valor de this.movePx e, quando clicar no botão de scroll-left, ele vai reposicionar a página para esse offset que estaria fora da lista (esse offset poderia ser infinito)
+      }
+    }
+    
   }
 
 }
