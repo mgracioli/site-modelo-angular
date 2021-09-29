ffce4e606079b0a082e336f9c86fc259625d2426
mgracioli <michel-rg@hotmail.com>
2021-09-28 22:02:52 -0300

    cont. implementação dascrol carousel

--- src/app/shared/components/scroll-carrousel/scroll-carrousel.component.ts
+++ src/app/shared/components/scroll-carrousel/scroll-carrousel.component.ts
@@ -2,56 +2,61 @@ import { Component, OnInit } from '@angular/core';
 import { cardProduct } from './card-product';
 
 @Component({
-  selector: 'app-scroll-carrousel',
-  templateUrl: './scroll-carrousel.component.html',
-  styleUrls: ['./scroll-carrousel.component.scss']
+	selector: 'app-scroll-carrousel',
+	templateUrl: './scroll-carrousel.component.html',
+	styleUrls: ['./scroll-carrousel.component.scss']
 })
 export class ScrollCarrouselComponent implements OnInit {
 
-  
-  cardProduct: cardProduct = {
-    cellName: '',
-    price: '',
-    storeName: ''
-  }
-  movePx: number = 0;
+	//produtos a serem exibidos na lista de produtos
+	cardProducts: cardProduct[] = []
 
-  constructor() { }
+	movePx: number = 0;
 
-  ngOnInit(): void {
+	constructor() { }
 
-    //inicializando a variável para servir de exemplo
-    this.cardProduct.cellName = 'Iphone X'
-    this.cardProduct.price = '1257,30'
-    this.cardProduct.storeName = 'Electronics & More'
-  }
+	ngOnInit(): void {
+		//função para preencher a lista de produtos
+		this.preencheCardList()	
+	}
 
-  moveProducts(side: string){
-    const carouselList = document.getElementById('carousel-list')
-    
-    if(side == "left"){
-      this.movePx-=450
+	/***************************************************************/
+	/* Função para preencher a lista de produtos a titulo de exemplo
+	/***************************************************************/
+	preencheCardList(){
+		for (let i = 0; i < 15; i++) {
+			this.cardProducts.push({
+				cellName: 'Iphone X',
+				price: '1257,30',
+				storeName: 'Electronics & More'
+			})
+		}
 
-      if((carouselList!.offsetWidth + this.movePx) > 0){
-        //console.log("width do carossel: "+carouselList?.offsetWidth)
-        //console.log("movePx: "+this.movePx)
-        console.log("movePx+carList: "+carouselList!.offsetWidth + this.movePx)
-        carouselList!.style.left = this.movePx+"px"
-      }else{
-        this.movePx = carouselList!.offsetWidth;  //0 para travar o card-list senão ele vai ficar aumentando o valor de this.movePx e, quando clicar no botão de scroll-left, ele vai reposicionar a página para esse offset que estaria fora da lista (esse offset poderia ser infinito)
-        console.log("movePx final: "+this.movePx)
-      }    
-    }else if(side == "right"){
-      this.movePx+=450
+	}
 
-      //offsetLeft retorna a distancia, em pixels, do carousel-list com relação ao carousel-frame
-      if(carouselList!.offsetLeft != 0){
-        carouselList!.style.left = this.movePx+"px"
-      }else{
-        this.movePx = 0;  //0 para travar o card-list senão ele vai ficar aumentando o valor de this.movePx e, quando clicar no botão de scroll-left, ele vai reposicionar a página para esse offset que estaria fora da lista (esse offset poderia ser infinito)
-      }
-    }
-    
-  }
+	/***************************************************************/
+	/* Função para mover a lista de produtos
+	/***************************************************************/
+	moveProducts(side: string) {
+		const carouselList = document.getElementById('carousel-list')
+
+		if (side == "left") {
+			if ((this.movePx * -1) < carouselList!.scrollWidth) {
+				this.movePx-=600
+
+				//console.log("this.MovePx: " + this.movePx)
+				//console.log('scrollWidth: ' + carouselList!.scrollWidth)
+				carouselList!.style.left = this.movePx + "px"
+			}
+		} else if (side == "right") {
+			//se this.movePx < 0 quer dizer que existem produtos a serem mostrados do lado esquerdo, se for = 0 é porque não tem (não vai ser nunca > 0 pq no momento que que this.movePx for > 0 o programa entra no else e faz o seu valor ser = 0)
+			if (this.movePx < 0) {
+				this.movePx+=600
+				carouselList!.style.left = this.movePx + "px"
+			} else {
+				this.movePx = 0;  //0 para travar o card-list senão ele vai ficar aumentando o valor de this.movePx e, quando clicar no botão de scroll-left, ele vai reposicionar a página para esse offset que estaria fora da lista (esse offset poderia ser infinito)
+			}
+		}
+	}
 
 }
