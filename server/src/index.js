502759fa1c72bad9f736b6a8affe9f72cadbbfef
mgracioli <michel-rg@hotmail.com>
2021-10-02 21:39:57 -0300

    implementação do layout da barra de busca e ajustes no carrossel de produtos

--- server/src/index.js
+++ server/src/index.js
@@ -5,11 +5,12 @@ const express = require('express');
 
 const app = express();
 
-//middlewares
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+//middlewares (middlewares are methods/functions/operations that are called BETWEEN processing the Request and sending the Response in your application method)
+//You NEED express.json() and express.urlencoded() only in POST and PUT requests, because in both these requests you are sending data (in the form of some data object) to the server and you are asking the server to accept or store that data (object), which is enclosed in the body (i.e. req.body) of that (POST or PUT) Request
+app.use(express.json());    //express.json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object. This method is called as a middleware in your application using the code: app.use(express.json());
+app.use(express.urlencoded({extended: true})) //express.urlencoded() is a method inbuilt in express to recognize the incoming Request Object as strings or arrays. This method is called as a middleware in your application using the code: app.use(express.urlencoded());
 
-//Endpoint para o download de arquivos PDF (localhost:8000/downloadPDF)
+//Endpoint para o download de arquivos PDF (localhost:8000/downloadMenuPDF)
 app.get('/downloadMenuPDF', (req, res) => { // /downloadMenuPDF é a chamada do backend (rota do backend) para fazer o download, lá no arquivo.component.ts eu tenho q redirecionar algum botão ou link para essa rota. O pathRewrite lá do proxy existe por conta dessa rota, isso pq ela está declarada aqui como /downloadMenuPDF e não /api/downloadMenuPDF
     res.download('src/files/menu.pdf'); //faz o download do arquivo menu.pdf que está na pasta 'files'
 });
--- src/app/shared/components/scroll-carrousel/scroll-carrousel.component.ts
+++ src/app/shared/components/scroll-carrousel/scroll-carrousel.component.ts
@@ -11,7 +11,9 @@ export class ScrollCarrouselComponent implements OnInit {
 	//produtos a serem exibidos na lista de produtos
 	cardProducts: cardProduct[] = []
 
-	movePx: number = 0;
+	movePx: number = 0
+	showButtonLeft = false	//mostra ou esconde os botões do carrossel de produtos
+	showButtonRight = true	//mostra ou esconde os botões do carrossel de produtos
 
 	constructor() { }
 
@@ -26,8 +28,8 @@ export class ScrollCarrouselComponent implements OnInit {
 	preencheCardList(){
 		for (let i = 0; i < 15; i++) {
 			this.cardProducts.push({
-				cellName: 'Iphone X 32GB Camera 55mpx Rose Gold, Silver or Black and much more',
-				price: '1257,30',
+				cellName: 'Iphone X 32GB Camera 55mpx Rose Gold, Silver or Black Gorilla Glass',
+				price: '1.257,30',
 				storeName: 'Electronics & More State Avenue'
 			})
 		}
@@ -38,24 +40,49 @@ export class ScrollCarrouselComponent implements OnInit {
 	/***************************************************************/
 	moveProducts(side: string) {
 		const carouselList = document.getElementById('carousel-list')
+		const carouselFrame = document.getElementById('carousel-frame')
+
+
+		console.log("this.MovePx: " + this.movePx)
 
 		if (side == "left") {
-			if ((this.movePx * -1) < carouselList!.scrollWidth) {
+			if ((this.movePx * -1) < carouselFrame!.clientWidth) {
 				this.movePx-=600
-
-				//console.log("this.MovePx: " + this.movePx)
-				//console.log('scrollWidth: ' + carouselList!.scrollWidth)
+				this.showButtons(this.movePx)
 				carouselList!.style.left = this.movePx + "px"
 			}
 		} else if (side == "right") {
 			//se this.movePx < 0 quer dizer que existem produtos a serem mostrados do lado esquerdo, se for = 0 é porque não tem (não vai ser nunca > 0 pq no momento que que this.movePx for > 0 o programa entra no else e faz o seu valor ser = 0)
 			if (this.movePx < 0) {
 				this.movePx+=600
+				this.showButtons(this.movePx)
 				carouselList!.style.left = this.movePx + "px"
 			} else {
-				this.movePx = 0;  //0 para travar o card-list senão ele vai ficar aumentando o valor de this.movePx e, quando clicar no botão de scroll-left, ele vai reposicionar a página para esse offset que estaria fora da lista (esse offset poderia ser infinito)
+				this.movePx = 0  //0 para travar o card-list senão ele vai ficar aumentando o valor de this.movePx e, quando clicar no botão de scroll-left, ele vai reposicionar a página para esse offset que estaria fora da lista (esse offset poderia ser infinito)
 			}
 		}
 	}
+
+	showButtons(movePx: number){
+		const carouselFrame = document.getElementById('carousel-frame')
+
+		console.log('carousel-frame: '+carouselFrame?.clientWidth)
+		console.log('movePx: '+movePx)
+
+		if(movePx < 0){
+			this.showButtonLeft = true		
+			
+			if((movePx * -1) < carouselFrame!.clientWidth){
+				this.showButtonRight = true
+			}else{
+				this.showButtonRight = false
+			}
+			
+		}else{
+			this.showButtonLeft = false
+			this.showButtonRight = true
+		}	
+	}
+	
 
 }
